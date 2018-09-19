c4eaec89c076a283f4d8804ff6f2d70484573cae
[fanshun91] <761631866@qq.com>
2018-09-19 14:26:15 +0800

    use @vue/cli

---
+++ Vuetodo_basic/cli-todolist/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="inputVal">
+    <button @click="handleSubmit">提交</button>
+    <ul>
+      <todo-item 
+        v-for="(item, index) of list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="handleDelete"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem";
+
+export default {
+  name: "app",
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      inputVal: "",
+      list: []
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.list.push(this.inputVal);
+      this.inputVal = "";
+    },
+    handleDelete(index) {
+      this.list.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
